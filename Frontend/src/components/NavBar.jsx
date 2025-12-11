6dca5adeb5b7a516c73ecc541d068492f0d3c7aa
LeOn-Opmac <leoncampo2707@gmail.com>
2025-12-11 11:39:26 -0500

    boostrap

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -1,16 +1,23 @@
 import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 
 function NavBar() {
     return (
-        <div className="navbar">
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/register">Register</Link></li>
-                </ul>
-            </nav>
-        </div>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="/">Navbar</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/login">Login</Nav.Link> 
+                        <Nav.Link href="/register">Register</Nav.Link>  
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
